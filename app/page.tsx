ea6ed632e2ba15017b7c4ac234bd738f341a5aa7
Abdurrahman <abdurrahman.abdurrahman1@muf.co.id>
2024-06-15 14:31:45 +0700

    add uploadthing lib and config tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <h1 className="text-xl">Duar</h1>
+
       {/* bisa menambahkan style sendiri secara langsung */}
       <Button variant="destructive" className="px-10">Delete</Button>
     </main>
