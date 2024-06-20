fdf63996704b4e785168cab335ffbc40fbdc42ca
Abdurrahman <abdurrahman.abdurrahman1@muf.co.id>
2024-06-20 11:34:25 +0700

    fix error issue when deploying

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -29,12 +29,12 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
 
           <div className="flex w-full flex-col gap-8 p-5 md:p-10">
             <div className="flex flex-col gap-6">
-              <h2 className='h2-bold'>{event.title}</h2>
+              <h2 className="h2-bold">{event.title}</h2>
 
               <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                 <div className="flex gap-3">
                   <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
-                    {event.isFree ? 'FREE' : `$${event.price}`}
+                    {event.isFree ? "FREE" : `$${event.price}`}
                   </p>
                   <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
                     {event.category.name}
@@ -51,7 +51,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
             {/* <CheckoutButton event={event} /> */}
 
             <div className="flex flex-col gap-5">
-              <div className='flex gap-2 md:gap-3'>
+              <div className="flex gap-2 md:gap-3">
                 <Image src="/assets/icons/calendar.svg" alt="calendar" width={32} height={32} />
                 <div className="p-medium-16 lg:p-regular-20 flex flex-col">
                   <p>
@@ -72,7 +72,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
             </div>
 
             <div className="flex flex-col gap-2">
-              <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
+              <p className="p-bold-20 text-grey-600">What You&apos;ll Learn:</p>
               <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
               <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">{event.url}</p>
             </div>
